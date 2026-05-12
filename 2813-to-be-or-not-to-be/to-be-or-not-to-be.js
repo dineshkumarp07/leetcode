88e5517ed65ba77dafd67cc0ac201e2c92f9743a
Dinesh kumar.P <dineshkumarp.23it@kongu.edu>
2026-05-12 14:30:15 +0530

    Time: 51 ms (15.82%) | Memory: 53.8 MB (33.14%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,17 @@
+var expect = function(val) {
+    return {
+        toBe: function(val2) {
+            if (val !== val2) {
+                throw new Error("Not Equal");
+            }
+            return true;
+        },
+        
+        notToBe: function(val2) {
+            if (val === val2) {
+                throw new Error("Equal");
+            }
+            return true;
+        }
+    };
+};
